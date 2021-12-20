4c2df722bc250806ee0bb1549d630183bbcd37cc
Dmitry Shepetov <55286003+DmitryShepetov@users.noreply.github.com>
2021-12-20 20:08:30 +0300

    Add files via upload

---
+++ WatchesShop/Migrations/20211220165630_WatchesShopApi.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WatchesShop.Migrations
+{
+    public partial class WatchesShopApi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    categoryName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    desc = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    delivery = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    payment = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    adress = table.Column<string>(type: "nvarchar(35)", maxLength: 35, nullable: false),
+                    phone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    email = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    dateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Watch",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    shortDesc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    longDesc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    price = table.Column<int>(type: "int", nullable: false),
+                    isFavourite = table.Column<bool>(type: "bit", nullable: false),
+                    img = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    newWatch = table.Column<bool>(type: "bit", nullable: false),
+                    available = table.Column<bool>(type: "bit", nullable: false),
+                    Categoryid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Watch", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Watch_Category_Categoryid",
+                        column: x => x.Categoryid,
+                        principalTable: "Category",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    orderId = table.Column<int>(type: "int", nullable: false),
+                    watchId = table.Column<int>(type: "int", nullable: false),
+                    price = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Order_orderId",
+                        column: x => x.orderId,
+                        principalTable: "Order",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Watch_watchId",
+                        column: x => x.watchId,
+                        principalTable: "Watch",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShopCartItems",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    watchid = table.Column<int>(type: "int", nullable: true),
+                    price = table.Column<int>(type: "int", nullable: false),
+                    ShopCartId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShopCartItems", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_ShopCartItems_Watch_watchid",
+                        column: x => x.watchid,
+                        principalTable: "Watch",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WatchImage",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    img = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    Watchid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WatchImage", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_WatchImage_Watch_Watchid",
+                        column: x => x.Watchid,
+                        principalTable: "Watch",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_orderId",
+                table: "OrderDetails",
+                column: "orderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_watchId",
+                table: "OrderDetails",
+                column: "watchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShopCartItems_watchid",
+                table: "ShopCartItems",
+                column: "watchid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Watch_Categoryid",
+                table: "Watch",
+                column: "Categoryid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WatchImage_Watchid",
+                table: "WatchImage",
+                column: "Watchid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "ShopCartItems");
+
+            migrationBuilder.DropTable(
+                name: "WatchImage");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Watch");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
